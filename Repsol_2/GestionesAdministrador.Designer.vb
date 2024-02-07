0f937a650adeb28138a6cb639567943e3717ebea
y1s1s <yisusnl420@gmail.com>
2024-02-07 11:39:58 +0100

    Paneles con botones de acciones agregados pero sin programar

--- Repsol_2/GestionesAdministrador.Designer.vb
+++ Repsol_2/GestionesAdministrador.Designer.vb
@@ -37,14 +37,32 @@ Partial Class GestionesAdministrador
         Me.btnVerTodosEmpleados = New System.Windows.Forms.Button()
         Me.btnSeleccionarUnEmpleado = New System.Windows.Forms.Button()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
+        Me.btnEliminarCliente = New System.Windows.Forms.Button()
+        Me.btnModificarCliente = New System.Windows.Forms.Button()
+        Me.btnVerTodosClientes = New System.Windows.Forms.Button()
+        Me.btnVerCliente = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.btnEliminarProducto = New System.Windows.Forms.Button()
+        Me.btnModificarProducto = New System.Windows.Forms.Button()
+        Me.btnVerTodosProductos = New System.Windows.Forms.Button()
+        Me.btnVerProducto = New System.Windows.Forms.Button()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.btnEliminarGas = New System.Windows.Forms.Button()
+        Me.btnModificarGasolina = New System.Windows.Forms.Button()
+        Me.btnVerTodasGasolinas = New System.Windows.Forms.Button()
+        Me.btnVerGasolina = New System.Windows.Forms.Button()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.btnEliminarProveedor = New System.Windows.Forms.Button()
+        Me.btnModificarProveedor = New System.Windows.Forms.Button()
+        Me.btnVerTodosProveedores = New System.Windows.Forms.Button()
+        Me.btnVerProveedor = New System.Windows.Forms.Button()
         Me.FlowLayoutPanel1.SuspendLayout()
         Me.MenuStrip1.SuspendLayout()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
         Me.SuspendLayout()
         '
         'FlowLayoutPanel1
@@ -175,56 +193,200 @@ Partial Class GestionesAdministrador
         '
         'Panel2
         '
-        Me.Panel2.Controls.Add(Me.Button1)
-        Me.Panel2.Controls.Add(Me.Button2)
-        Me.Panel2.Controls.Add(Me.Button3)
-        Me.Panel2.Controls.Add(Me.Button5)
+        Me.Panel2.Controls.Add(Me.btnEliminarCliente)
+        Me.Panel2.Controls.Add(Me.btnModificarCliente)
+        Me.Panel2.Controls.Add(Me.btnVerTodosClientes)
+        Me.Panel2.Controls.Add(Me.btnVerCliente)
         Me.Panel2.Location = New System.Drawing.Point(183, 138)
         Me.Panel2.Name = "Panel2"
         Me.Panel2.Size = New System.Drawing.Size(196, 51)
         Me.Panel2.TabIndex = 4
         '
-        'Button1
+        'btnEliminarCliente
         '
-        Me.Button1.Location = New System.Drawing.Point(98, 25)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 23)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Eliminar Emp"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnEliminarCliente.Location = New System.Drawing.Point(98, 25)
+        Me.btnEliminarCliente.Name = "btnEliminarCliente"
+        Me.btnEliminarCliente.Size = New System.Drawing.Size(94, 23)
+        Me.btnEliminarCliente.TabIndex = 3
+        Me.btnEliminarCliente.Text = "Eliminar Cli"
+        Me.btnEliminarCliente.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btnModificarCliente
         '
-        Me.Button2.Location = New System.Drawing.Point(98, 0)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(94, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Mod. Emp"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btnModificarCliente.Location = New System.Drawing.Point(98, 0)
+        Me.btnModificarCliente.Name = "btnModificarCliente"
+        Me.btnModificarCliente.Size = New System.Drawing.Size(94, 23)
+        Me.btnModificarCliente.TabIndex = 2
+        Me.btnModificarCliente.Text = "Mod. Cli"
+        Me.btnModificarCliente.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnVerTodosClientes
         '
-        Me.Button3.Location = New System.Drawing.Point(3, 25)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(89, 23)
-        Me.Button3.TabIndex = 1
-        Me.Button3.Text = "Ver Todos"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnVerTodosClientes.Location = New System.Drawing.Point(3, 25)
+        Me.btnVerTodosClientes.Name = "btnVerTodosClientes"
+        Me.btnVerTodosClientes.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerTodosClientes.TabIndex = 1
+        Me.btnVerTodosClientes.Text = "Ver Todos"
+        Me.btnVerTodosClientes.UseVisualStyleBackColor = True
         '
-        'Button5
+        'btnVerCliente
         '
-        Me.Button5.Location = New System.Drawing.Point(3, 0)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(89, 23)
-        Me.Button5.TabIndex = 0
-        Me.Button5.Text = "Ver Empleado"
-        Me.Button5.UseVisualStyleBackColor = True
+        Me.btnVerCliente.Location = New System.Drawing.Point(3, 0)
+        Me.btnVerCliente.Name = "btnVerCliente"
+        Me.btnVerCliente.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerCliente.TabIndex = 0
+        Me.btnVerCliente.Text = "Ver Cliente"
+        Me.btnVerCliente.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.btnEliminarProducto)
+        Me.Panel3.Controls.Add(Me.btnModificarProducto)
+        Me.Panel3.Controls.Add(Me.btnVerTodosProductos)
+        Me.Panel3.Controls.Add(Me.btnVerProducto)
+        Me.Panel3.Location = New System.Drawing.Point(183, 195)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(196, 51)
+        Me.Panel3.TabIndex = 5
+        '
+        'btnEliminarProducto
+        '
+        Me.btnEliminarProducto.Location = New System.Drawing.Point(98, 25)
+        Me.btnEliminarProducto.Name = "btnEliminarProducto"
+        Me.btnEliminarProducto.Size = New System.Drawing.Size(94, 23)
+        Me.btnEliminarProducto.TabIndex = 3
+        Me.btnEliminarProducto.Text = "Eliminar Prod"
+        Me.btnEliminarProducto.UseVisualStyleBackColor = True
+        '
+        'btnModificarProducto
+        '
+        Me.btnModificarProducto.Location = New System.Drawing.Point(98, 0)
+        Me.btnModificarProducto.Name = "btnModificarProducto"
+        Me.btnModificarProducto.Size = New System.Drawing.Size(94, 23)
+        Me.btnModificarProducto.TabIndex = 2
+        Me.btnModificarProducto.Text = "Mod. Prod"
+        Me.btnModificarProducto.UseVisualStyleBackColor = True
+        '
+        'btnVerTodosProductos
+        '
+        Me.btnVerTodosProductos.Location = New System.Drawing.Point(3, 25)
+        Me.btnVerTodosProductos.Name = "btnVerTodosProductos"
+        Me.btnVerTodosProductos.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerTodosProductos.TabIndex = 1
+        Me.btnVerTodosProductos.Text = "Ver Todos"
+        Me.btnVerTodosProductos.UseVisualStyleBackColor = True
+        '
+        'btnVerProducto
+        '
+        Me.btnVerProducto.Location = New System.Drawing.Point(3, 0)
+        Me.btnVerProducto.Name = "btnVerProducto"
+        Me.btnVerProducto.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerProducto.TabIndex = 0
+        Me.btnVerProducto.Text = "Ver Producto"
+        Me.btnVerProducto.UseVisualStyleBackColor = True
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.btnEliminarGas)
+        Me.Panel4.Controls.Add(Me.btnModificarGasolina)
+        Me.Panel4.Controls.Add(Me.btnVerTodasGasolinas)
+        Me.Panel4.Controls.Add(Me.btnVerGasolina)
+        Me.Panel4.Location = New System.Drawing.Point(183, 252)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(196, 51)
+        Me.Panel4.TabIndex = 6
+        '
+        'btnEliminarGas
+        '
+        Me.btnEliminarGas.Location = New System.Drawing.Point(98, 25)
+        Me.btnEliminarGas.Name = "btnEliminarGas"
+        Me.btnEliminarGas.Size = New System.Drawing.Size(94, 23)
+        Me.btnEliminarGas.TabIndex = 3
+        Me.btnEliminarGas.Text = "Eliminar Gas"
+        Me.btnEliminarGas.UseVisualStyleBackColor = True
+        '
+        'btnModificarGasolina
+        '
+        Me.btnModificarGasolina.Location = New System.Drawing.Point(98, 0)
+        Me.btnModificarGasolina.Name = "btnModificarGasolina"
+        Me.btnModificarGasolina.Size = New System.Drawing.Size(94, 23)
+        Me.btnModificarGasolina.TabIndex = 2
+        Me.btnModificarGasolina.Text = "Mod. Gas"
+        Me.btnModificarGasolina.UseVisualStyleBackColor = True
+        '
+        'btnVerTodasGasolinas
+        '
+        Me.btnVerTodasGasolinas.Location = New System.Drawing.Point(3, 25)
+        Me.btnVerTodasGasolinas.Name = "btnVerTodasGasolinas"
+        Me.btnVerTodasGasolinas.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerTodasGasolinas.TabIndex = 1
+        Me.btnVerTodasGasolinas.Text = "Ver Todos"
+        Me.btnVerTodasGasolinas.UseVisualStyleBackColor = True
+        '
+        'btnVerGasolina
+        '
+        Me.btnVerGasolina.Location = New System.Drawing.Point(3, 0)
+        Me.btnVerGasolina.Name = "btnVerGasolina"
+        Me.btnVerGasolina.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerGasolina.TabIndex = 0
+        Me.btnVerGasolina.Text = "Ver Gasolina"
+        Me.btnVerGasolina.UseVisualStyleBackColor = True
+        '
+        'Panel5
+        '
+        Me.Panel5.Controls.Add(Me.btnEliminarProveedor)
+        Me.Panel5.Controls.Add(Me.btnModificarProveedor)
+        Me.Panel5.Controls.Add(Me.btnVerTodosProveedores)
+        Me.Panel5.Controls.Add(Me.btnVerProveedor)
+        Me.Panel5.Location = New System.Drawing.Point(183, 309)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(196, 51)
+        Me.Panel5.TabIndex = 7
+        '
+        'btnEliminarProveedor
+        '
+        Me.btnEliminarProveedor.Location = New System.Drawing.Point(98, 25)
+        Me.btnEliminarProveedor.Name = "btnEliminarProveedor"
+        Me.btnEliminarProveedor.Size = New System.Drawing.Size(94, 23)
+        Me.btnEliminarProveedor.TabIndex = 3
+        Me.btnEliminarProveedor.Text = "Eliminar Prov"
+        Me.btnEliminarProveedor.UseVisualStyleBackColor = True
+        '
+        'btnModificarProveedor
+        '
+        Me.btnModificarProveedor.Location = New System.Drawing.Point(98, 0)
+        Me.btnModificarProveedor.Name = "btnModificarProveedor"
+        Me.btnModificarProveedor.Size = New System.Drawing.Size(94, 23)
+        Me.btnModificarProveedor.TabIndex = 2
+        Me.btnModificarProveedor.Text = "Mod. Prov"
+        Me.btnModificarProveedor.UseVisualStyleBackColor = True
+        '
+        'btnVerTodosProveedores
+        '
+        Me.btnVerTodosProveedores.Location = New System.Drawing.Point(3, 25)
+        Me.btnVerTodosProveedores.Name = "btnVerTodosProveedores"
+        Me.btnVerTodosProveedores.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerTodosProveedores.TabIndex = 1
+        Me.btnVerTodosProveedores.Text = "Ver Todos"
+        Me.btnVerTodosProveedores.UseVisualStyleBackColor = True
+        '
+        'btnVerProveedor
+        '
+        Me.btnVerProveedor.Location = New System.Drawing.Point(3, 0)
+        Me.btnVerProveedor.Name = "btnVerProveedor"
+        Me.btnVerProveedor.Size = New System.Drawing.Size(89, 23)
+        Me.btnVerProveedor.TabIndex = 0
+        Me.btnVerProveedor.Text = "Ver Proveedor"
+        Me.btnVerProveedor.UseVisualStyleBackColor = True
         '
         'GestionesAdministrador
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1904, 1041)
+        Me.Controls.Add(Me.Panel5)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.FlowLayoutPanel1)
@@ -237,6 +399,9 @@ Partial Class GestionesAdministrador
         Me.MenuStrip1.PerformLayout()
         Me.Panel1.ResumeLayout(False)
         Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel5.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -257,8 +422,23 @@ Partial Class GestionesAdministrador
     Friend WithEvents btnVerTodosEmpleados As Button
     Friend WithEvents btnSeleccionarUnEmpleado As Button
     Friend WithEvents Panel2 As Panel
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button5 As Button
+    Friend WithEvents btnEliminarCliente As Button
+    Friend WithEvents btnModificarCliente As Button
+    Friend WithEvents btnVerTodosClientes As Button
+    Friend WithEvents btnVerCliente As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents btnEliminarProducto As Button
+    Friend WithEvents btnModificarProducto As Button
+    Friend WithEvents btnVerTodosProductos As Button
+    Friend WithEvents btnVerProducto As Button
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents btnEliminarGas As Button
+    Friend WithEvents btnModificarGasolina As Button
+    Friend WithEvents btnVerTodasGasolinas As Button
+    Friend WithEvents btnVerGasolina As Button
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents btnEliminarProveedor As Button
+    Friend WithEvents btnModificarProveedor As Button
+    Friend WithEvents btnVerTodosProveedores As Button
+    Friend WithEvents btnVerProveedor As Button
 End Class
