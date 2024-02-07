b47dbbc38f5750e0a44dca1c129932e9df5843d0
y1s1s <yisusnl420@gmail.com>
2024-02-07 09:29:14 +0100

    prueba conflictos

--- LibreriaDeClasesRepsol/Class1.vb
+++ LibreriaDeClasesRepsol/Class1.vb
@@ -15,6 +15,18 @@
             Return valido
         End Function
 
+        Public Function validarCaraferes(ByVal nombre As String) As Boolean
+            Dim valido As Boolean = True
+            Dim caracteresProhibidos As String = "(){}[]!¡¿?*+-,.;:_=|@#~¬^`´¨ºª\%&$€£<>¡"
+            For i As Integer = 0 To caracteresProhibidos.Length - 1
+                If nombre.Contains(caracteresProhibidos(i)) Then
+                    valido = False
+                    Exit For
+                End If
+            Next
+            Return valido
+        End Function
+
         'Esta funcion lo que hace es validar que la longitud de los campos (textbox) no sea superior a 10 caracteres
         Public Function validarLongitud(ByVal nombre As String) As Boolean
             Dim valido As Boolean = True
